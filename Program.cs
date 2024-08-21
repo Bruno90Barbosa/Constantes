e883beb30aba85395ae09d7d2a2594bbb5237d08
Bruno Barbosa <brunobarbosa90@hotmail.com>
2024-08-21 15:44:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int primeiraGuerraMundial = 1914;
+            const float valorDePi = 3.14159265358979323846f;
+            const string CapitaldaItalia = "Roma";
+            const bool fimDeSemana = false;
+            const bool quartaFeira = true;
+
+            Console.WriteLine(primeiraGuerraMundial);
+            Console.WriteLine(valorDePi);
+            Console.WriteLine(CapitaldaItalia);
+            Console.WriteLine(fimDeSemana);
+            Console.WriteLine(quartaFeira);
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
